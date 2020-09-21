ed3405e777cd8cba622a021230b62e2d4379e806
Maya Engel <45845033+MayaComposer@users.noreply.github.com>
2020-09-21 11:50:39 +0200

    Update AutoColourTracks.lua

--- AutoColourTracks.lua
+++ AutoColourTracks.lua
@@ -2,16 +2,20 @@
 --Set the custom track color, color is OS dependent (i.e. ColorToNative(r,g,b).
 --Script written by Maya Engel (@MayaComposer)
 
+--instrument families with their members
 instruments = {
-  strings={"violin","cello","viola","contrabass"},
-  brass={"horn","trumpet","tuba","trombone"},
-  woodwinds={"flute","clarinet", "english horn","bassoon"} 
+  strings={"Violin","Violincello","Viola","Contrabass", "Cello", "cello", "Erhu", "Bass", "Strings"},
+  brass={"Horn","Trumpet","Tuba","Trombone"},
+  woodwinds={"Flute","Clarinet", "Bassoon", "Oboe", "English Horn", "Piccolo", "Cor Anglais"},
+  perc={"Timpani", "Drum", "Piano", "Harp", "Celesta"} 
   } 
   
+--colours for each instrument family as RGB values
 colours = {
-  strings={38, 70, 83},
-  brass={233, 196, 106},
-  woodwinds={231, 111, 81}
+  strings={157, 2, 8},
+  brass={255, 190, 11},
+  woodwinds={160, 196, 255},
+  perc={131, 56, 236}
   }
   
 
@@ -22,11 +26,11 @@ reaper.ShowConsoleMsg("\n".."amount of tracks: "..amount_of_tracks)
 --for i in all tracks do
 for i=0,amount_of_tracks-1 do
   --debug
-  reaper.ShowConsoleMsg("\n"..i)
+ 
   --get track
   local track = reaper.GetTrack(self, i)
   
-  reaper.ShowConsoleMsg("\n".."track: "..tostring(track))
+  
   
   --get track name
   local success, name = reaper.GetTrackName(track)
@@ -57,4 +61,14 @@ for i=0,amount_of_tracks-1 do
         reaper.ColorToNative(colours.woodwinds[1],colours.woodwinds[2],colours.woodwinds[3]))
       end
     end
+  for k, v in pairs(instruments.perc) do
+        --if track name has any of the instruments in its name, set colour
+        if string.find(name, v) then
+          reaper.SetTrackColor(track, 
+          reaper.ColorToNative(colours.perc[1],colours.perc[2],colours.perc[3]))
+        end
+      end
+    
+  --reaper.ShowConsoleMsg("\n"..i)
+  --reaper.ShowConsoleMsg("\n".."track: "..tostring(track))
 end
